1cdc540b1c8377d97a2bc483b81ae6ebc4c5bdb6
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-10-08 18:19:07 -0300

    weather app

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-//importo los estilos de MUI
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"; //-> Reinicia los estilos base, y aplica la fuente roboto
-//tema personalizado para MUI
-const theme = createTheme({
-  palette: {
-    mode: "light",
-    primary: {
-      main: "#101650",
-    },
-    secondary: {
-      main: "#880e4f",
-    },
-  },
-});
-
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
