506dda638c62aa5a5e0e43c5c1d613b9f6b77096
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-10-08 20:43:59 -0300

    weather app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,193 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Box, Container, TextField, Typography } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
+
+const API_WEATHER = `http://api.weatherapi.com/v1/current.json?key=${
+  import.meta.env.VITE_API_KEY
+}&q=`;
+
+const App = () => {
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+
+  const [weather, setWeather] = useState({
+    city: "",
+    country: "",
+    temp: "",
+    condition: "",
+    icon: "",
+    conditionText: "",
+    localTime: "",
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    /* deja el boton en cargando mientras hace la petición */
+    setLoading(true);
+    setError({
+      error: false,
+      message: error.message,
+    });
+
+    try {
+      /* .trim() limpia los espacios en blanco */
+      /* si no se escribe nada arroja el error */
+      if (!city.trim()) throw { message: "El campo ciudad es obligatorio" };
+
+      const response = await fetch(`${API_WEATHER}${city}`);
+      /* fetch devuelve la data */
+      const data = await response.json();
+
+      if (data.error) throw { message: data.error.message };
+
+      const localTime = data.location.localtime;
+
+      // Dividir la fecha y hora en partes separadas
+      const [fecha, hora] = localTime.split(" ");
+
+      // Dividir la fecha en partes separadas (año, mes y día)
+      const [año, mes, dia] = fecha.split("-");
+
+      // Convertir el mes de formato numérico a cadena
+      const meses = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      const mesString = meses[parseInt(mes, 10) - 1];
+
+      setWeather({
+        city: data.location.name,
+        country: data.location.country,
+        temp: data.current.temp_c,
+        condition: data.current.condition.code,
+        icon: data.current.condition.icon,
+        conditionText: data.current.condition.text,
+        localDate: `${mesString} ${dia}, ${parseInt(año, 10)}`,
+        localTime: `${hora}`,
+      });
+    } catch (error) {
+      setError({
+        error: true,
+        message: error.message,
+      });
+    } finally {
+      setLoading(false);
+    }
+    setCity("");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Container
+      maxWidth="xs"
+      sx={{ mt: 2 }}
+    >
+      <Typography
+        variant="h3"
+        component="h1"
+        textAlign="center"
+        gutterBottom
+      >
+        The Weather App
+      </Typography>
 
-export default App
+      <Box
+        sx={{ display: "grid", gap: 2, mt: 4 }}
+        component="form"
+        autoComplete="off"
+        onSubmit={onSubmit}
+      >
+        <TextField
+          id="city"
+          label="Ciudad"
+          variant="outlined"
+          size="small"
+          required
+          fullWidth
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          error={error.error}
+          helperText={error.message}
+        />
+
+        <LoadingButton
+          type="submit"
+          variant="contained"
+          loading={loading}
+          loadingIndicator="Buscando ciudad..."
+        >
+          Buscar
+        </LoadingButton>
+      </Box>
+
+      {weather.city && (
+        <Box sx={{ mt: 2, display: "grid", gap: 2, textAlign: "center" }}>
+          <Typography
+            variant="h4"
+            component="h2"
+          >
+            {weather.city}, {weather.country}
+          </Typography>
+          <Box
+            component="img"
+            src={weather.icon}
+            alt={weather.conditionText}
+            sx={{ margin: "0 auto" }}
+          />
+          <Typography
+            variant="h5"
+            component="h3"
+          >
+            {weather.temp} °C
+          </Typography>
+          <Typography
+            variant="h6"
+            component="h4"
+          >
+            {weather.conditionText}
+          </Typography>
+          <Typography
+            variant="h6"
+            component="h4"
+          >
+            {weather.localTime}hs
+          </Typography>
+          <Typography
+            variant="body1"
+            component="p"
+          >
+            {weather.localDate}
+          </Typography>
+        </Box>
+      )}
+
+      <Typography
+        textAlign="center"
+        sx={{ mt: 6, fontSize: "10px" }}
+      >
+        Powered by:{" "}
+        <a
+          href="https://www.weatherapi.com/"
+          title="Weather API"
+        >
+          WeatherAPI.com
+        </a>
+      </Typography>
+    </Container>
+  );
+};
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import CssBaseline from "@mui/material/CssBaseline";
+import { SnackbarProvider } from "notistack";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <SnackbarProvider>
+      <CssBaseline />
+      <App />
+    </SnackbarProvider>
+  </React.StrictMode>
+);
